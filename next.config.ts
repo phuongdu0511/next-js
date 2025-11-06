4d6f567c9419ca35eeb160919637520b7af4ea9a
Duong Phuong Duy <dev_crm@bitbucket.org>
2025-11-06 17:38:12 +0700

    done home layout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api-bigboy.duthanhduoc.com",
+        pathname: "/**",
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '4000',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
