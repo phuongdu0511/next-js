8056d54e59a977142e074ec0ccd74b3064d81891
Duong Phuong Duy <dev_crm@bitbucket.org>
2025-11-06 15:22:54 +0700

    custom image qr code

--- components/qrcode-table.tsx
+++ components/qrcode-table.tsx
@@ -14,18 +14,38 @@ export default function QrCodeTable({
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
     const canvas = canvasRef.current;
+    if (!canvas) return;
+    canvas.height = width + 70;
+    canvas.width = width;
+    const canvasContext = canvas.getContext("2d")!;
+    canvasContext.fillStyle = "#fff";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    canvasContext.font = "20px Arial";
+    canvasContext.textAlign = "center";
+    canvasContext.fillStyle = "#000";
+    canvasContext.fillText(
+      `Bàn số ${tableNumber}`,
+      canvas.width / 2,
+      canvas.width + 20
+    );
+    canvasContext.fillText(
+      `Quét mã QR để gọi món`,
+      canvas.width / 2,
+      canvas.width + 50
+    );
 
+    const virtualCanvas = document.createElement('canvas')
     QRCode.toCanvas(
-      canvas,
+      virtualCanvas,
       getTableLink({
         token,
         tableNumber,
       }),
       function (error) {
         if (error) console.error(error);
-        console.log("success!");
+        canvasContext.drawImage(virtualCanvas, 0, 0, width, width)
       }
     );
-  }, [token, tableNumber]);
+  }, [token, width ,tableNumber]);
   return <canvas ref={canvasRef} />;
 }
