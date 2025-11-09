d8555adcc83cf005f9d04d6f54973fec4ad701ca
Duong Phuong Duy <dev_crm@bitbucket.org>
2025-11-09 20:54:53 +0700

    update order api

--- queries/useOrder.tsx
+++ queries/useOrder.tsx
@@ -1,5 +1,5 @@
 import orderApiRequest from "@/apiRequests/order";
-import { UpdateOrderBodyType } from "@/schemaValidations/order.schema";
+import { GetOrdersQueryParamsType, UpdateOrderBodyType } from "@/schemaValidations/order.schema";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const useUpdateOrderMutation = () => {
@@ -12,9 +12,22 @@ export const useUpdateOrderMutation = () => {
   });
 };
 
-export const useGetOrderListQuery = () => {
+export const useGetOrderListQuery = (queryParams: GetOrdersQueryParamsType) => {
     return useQuery({
-        queryFn: orderApiRequest.getOrderList,
-        queryKey: ['orders']
+        queryFn: () => orderApiRequest.getOrderList(queryParams),
+        queryKey: ['orders', queryParams]
     })
+}
+
+export const useGetOrderDetailQuery = ({
+  id, enabled
+} : {
+  id: number
+  enabled: boolean
+}) => {
+  return useQuery({
+    queryFn: () => orderApiRequest.getOrderDetail(id),
+    queryKey: ['orders', id],
+    enabled
+  })
 }
