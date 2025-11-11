b53f4642807c189318a6f0ffdda79cdaa5b0b856
Duong Phuong Duy <dev_crm@bitbucket.org>
2025-11-11 19:55:09 +0700

    add indicator api

--- app/manage/dashboard/revenue-line-chart.tsx
+++ app/manage/dashboard/revenue-line-chart.tsx
@@ -1,61 +1,35 @@
-'use client'
+"use client";
 
-import { TrendingUp } from 'lucide-react'
-import { CartesianGrid, Line, LineChart, XAxis } from 'recharts'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart'
-import { format, parse } from 'date-fns'
+import { TrendingUp } from "lucide-react";
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { format, parse } from "date-fns";
+import { DashboardIndicatorResType } from "@/schemaValidations/indicator.schema";
 const chartConfig = {
   desktop: {
-    label: 'Desktop',
-    color: 'hsl(var(--chart-1))'
-  }
-} satisfies ChartConfig
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig;
 
-export function RevenueLineChart() {
-  // fake 10 item
-  const chartData = [
-    {
-      date: '01/01/2024',
-      revenue: 1000
-    },
-    {
-      date: '02/01/2024',
-      revenue: 2000
-    },
-    {
-      date: '03/01/2024',
-      revenue: 1500
-    },
-    {
-      date: '04/01/2024',
-      revenue: 3000
-    },
-    {
-      date: '05/01/2024',
-      revenue: 2500
-    },
-    {
-      date: '06/01/2024',
-      revenue: 4000
-    },
-    {
-      date: '07/01/2024',
-      revenue: 3500
-    },
-    {
-      date: '08/01/2024',
-      revenue: 5000
-    },
-    {
-      date: '09/01/2024',
-      revenue: 4500
-    },
-    {
-      date: '10/01/2024',
-      revenue: 6000
-    }
-  ]
+export function RevenueLineChart({
+  chartData,
+}: {
+  chartData: DashboardIndicatorResType["data"]["revenueByDate"];
+}) {
   return (
     <Card>
       <CardHeader>
@@ -69,32 +43,42 @@ export function RevenueLineChart() {
             data={chartData}
             margin={{
               left: 12,
-              right: 12
+              right: 12,
             }}
           >
             <CartesianGrid vertical={false} />
             <XAxis
-              dataKey='date'
+              dataKey="date"
               tickLine={false}
               axisLine={false}
               tickMargin={8}
               tickFormatter={(value) => {
                 if (chartData.length < 8) {
-                  return value
+                  return value;
                 }
                 if (chartData.length < 33) {
-                  const date = parse(value, 'dd/MM/yyyy', new Date())
-                  return format(date, 'dd')
+                  const date = parse(value, "dd/MM/yyyy", new Date());
+                  return format(date, "dd");
                 }
-                return ''
+                return "";
               }}
             />
-            <ChartTooltip cursor={false} content={<ChartTooltipContent indicator='dashed' />} />
-            <Line dataKey='revenue' type='linear' stroke='var(--color-desktop)' strokeWidth={2} dot={false} />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="dashed" />}
+            />
+            <Line
+              dataKey="revenue"
+              name="Doanh thu"
+              type="linear"
+              stroke="var(--color-desktop)"
+              strokeWidth={2}
+              dot={false}
+            />
           </LineChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className='flex-col items-start gap-2 text-sm'>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
         {/* <div className='flex gap-2 font-medium leading-none'>
           Trending up by 5.2% this month <TrendingUp className='h-4 w-4' />
         </div>
@@ -103,5 +87,5 @@ export function RevenueLineChart() {
         </div> */}
       </CardFooter>
     </Card>
-  )
+  );
 }
